1e54477343ef0e9c95d81cad4b2c893d5fa7a659
yassin <Benchayadamohamedyassin@mahdia.r-iset.tn>
2025-11-25 08:18:24 +0100

    feat: add user login (docker-1)

--- app.py
+++ app.py
@@ -11,3 +11,4 @@ if __name__ == '__main__':
     ##comment: This is a simple Flask application that returns 'Hello, World! yoooooo' when accessed at the root URL.
     ##comment: The application is set to run in debug mode for easier development and troubleshooting.
     ##comment: To run the application, execute this script and navigate to http://
+    ##"localhost:5000/" in your web browser.
