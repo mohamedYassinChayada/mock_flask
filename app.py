53996f8f4403c32a794edd48418d688f8bfda8d4
yassin <Benchayadamohamedyassin@mahdia.r-iset.tn>
2025-11-25 09:46:36 +0100

    aaza (docker-10)

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')=
+@app.route('/')
+def hello_world():
+    return 'Hello, World! yoooooo'
+
 if __name__ == '__main__':
     app.run(debug=True)
     ##comment: This is a simple Flask application that returns 'Hello, World! yoooooo' when accessed at the root URL.
