dd0836d8651ae99033db7679cb17bb1dbaa9911b
yassin <Benchayadamohamedyassin@mahdia.r-iset.tn>
2025-12-01 18:12:33 +0100

    ????

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world(
-    return 'Hello, World! yoooooo'
+    retsurn 'Hello, World! yoooooo'
 
 if __name__ == '__main__':  
     app.run(debug=True)
